254be4e010708c1a3430235b403c8c5cc00f6736
Goia Ciprian <goia.ciprian14@gmail.com>
2021-12-10 17:14:03 +0200

    terminat adaugat modele + terminat adaugat modele in pagina de admin

--- apartamente_api/models.py
+++ apartamente_api/models.py
@@ -1,5 +1,6 @@
+from django import db
 from django.db import models
-
+from apartamente_app.settings import AUTH_USER_MODEL
 # Create your models here.
 
 
@@ -19,20 +20,112 @@ class TipApartament(ModelBase):
     """
     Model for tip apartament
     """
-    Denumire = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'tip_apartament'
+
+    denumire = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return self.denumire
+
 
 class Apartament(ModelBase):
     """
     Model for apartament
     """
-    denumire = models.CharField(max_length=50)
-    adresa = models.CharField(max_length=50)
-    tip = models.ForeignKey(TipApartament, on_delete=models.CASCADE)
-    chirie = models.DecimalField(max_digits=10, decimal_places=2)
-    descriere = models.TextField()
+    class Meta:
+        db_table = 'apartament'
+
+    denumire = models.CharField(max_length=50, null=False)
+    adresa = models.CharField(max_length=50, null=False)
+    tip = models.ForeignKey(
+        TipApartament, on_delete=models.CASCADE, null=False)
+    chirie = models.DecimalField(max_digits=10, decimal_places=2, )
+
+    owner = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+
+    aplicanti = models.ManyToManyField(
+        AUTH_USER_MODEL, through="AplicantiApartament", related_name='aplicanti')
+
+    locuitori = models.ManyToManyField(
+        AUTH_USER_MODEL, through="LocuitoriApartament", related_name='locuitori')
 
     def __str__(self):
-        return self.nume
+        return self.denumire
+
+    def get_tip(self):
+        return self.tip.denumire
+    get_tip.short_description = 'Tip'
+
+    def get_locuitori_number(self):
+        return self.locuitori.count()
+    get_locuitori_number.short_description = 'Locuitori No'
+
+    def get_aplicanti_number(self):
+        return self.aplicanti.count()
+    get_aplicanti_number.short_description = 'Aplicanti No'
+
+    def get_chirie(self):
+        return self.chirie
+    get_chirie.short_description = 'Chirie'
+
+
+class AplicantiApartament(ModelBase):
+    """
+    Model for AplicantiApartament
+    """
+    class Meta:
+        db_table = 'aplicanti_apartament'
+
+    apartament = models.ForeignKey(
+        Apartament, on_delete=models.CASCADE, null=False)
+    aplicant = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+
+    status = models.BooleanField(null=True)
+
+    def nume_aplicant(self):
+        return self.aplicant.first_name + ' ' + self.aplicant.last_name
+    nume_aplicant.short_description = 'Nume Aplicant'
+
+    def denumire_apartament(self):
+        return self.apartament.denumire
+    denumire_apartament.short_description = 'Apartament'
+
+    def data_aplicare(self):
+        return self.created_at
+    data_aplicare.short_description = 'Data Aplicare'
+
+    def __str__(self):
+        return f'{self.apartament.denumire} - {self.aplicant.first_name} {self.aplicant.last_name}'
+
+
+class LocuitoriApartament(ModelBase):
+    """
+    Model for LocuitoriApartament
+    """
+
+    class Meta:
+        db_table = 'locuitori_apartament'
+
+    apartament = models.ForeignKey(
+        Apartament, on_delete=models.CASCADE, null=False)
+    locuitor = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, )
+
+    locuieste_in_prezent = models.BooleanField(default=True)
+
+    def nume_locuitor(self):
+        return self.locuitor.first_name + ' ' + self.locuitor.last_name
+    nume_locuitor.short_description = 'Nume Locuitor'
+
+    def denumire_apartament(self):
+        return self.apartament.denumire
+    denumire_apartament.short_description = 'Apartament'
+
+    def data_inceput(self):
+        return self.created_at
+    data_inceput.short_description = 'Data Inceput'
+
+    def __str__(self):
+        return f'{self.apartament.denumire} - {self.locuitor.first_name} {self.locuitor.last_name}'
